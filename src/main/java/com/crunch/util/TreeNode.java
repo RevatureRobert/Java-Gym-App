4f48d9ae583fe39e0ecd17172f8220abfc967d00
Nick Z <nicolas.zimmerman@revature.net>
2021-02-25 09:48:46 -0700

    renamed Node class to avoid conflicts

---
+++ src/main/java/com/crunch/util/TreeNode.java
@@ -0,0 +1,22 @@
+package com.crunch.util;
+
+import com.crunch.model.User;
+
+public class TreeNode extends User {
+
+    private TreeNode left_node;
+    private TreeNode right_node;
+    private User value;
+
+    public TreeNode getLeftNode() {
+        return this.left_node;
+    }
+
+    public TreeNode getRightNode() {
+        return this.right_node;
+    }
+
+    
+
+
+}
